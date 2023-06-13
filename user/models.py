364cff19994c4f0c991b43cd6bacca43f9100e75
Talha Khan <123312136+talhakhan54@users.noreply.github.com>
2023-06-13 09:02:49 +0600

    __

--- user/models.py
+++ user/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
+from user.manager import UserManager
 # Create your models here.
+
+
+class CustomUser(AbstractUser):
+    phone_number = models.CharField(max_length=11,blank=True)
+    address = models.CharField(max_length=50,blank=True)
+    REQUIRED_FIELDS = []
+    objects = UserManager()
