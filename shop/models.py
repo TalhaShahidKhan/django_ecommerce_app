aa20fbacc83f605a395252d5eecb5af5f446a353
Talha Khan <123312136+talhakhan54@users.noreply.github.com>
2023-06-18 09:58:27 +0600

    __

--- shop/models.py
+++ shop/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.template.defaultfilters import slugify  
+
 # Create your models here.
 
 
@@ -6,9 +8,15 @@ from django.db import models
 class Product(models.Model):
   name = models.CharField(max_length=20,blank=True)
   des  = models.TextField(blank=True)
+  slug = models.SlugField(unique=True, blank=True, null=True)
   pic = models.ImageField(upload_to='shop/PrP')
   price = models.IntegerField(blank=True)
   featured = models.BooleanField(default=False, blank=True)
+  created_at = models.DateTimeField(auto_now_add=True,blank = True,null=True)
+  updated_at = models.DateTimeField(auto_now=True,blank = True,null=True)
+
+  class Meta:
+      ordering = ['-created_at']
 
 
   def is_featured(self):
@@ -18,13 +26,28 @@ class Product(models.Model):
     return self.name
   
 
+  def save(self,*args, **kwargs):
+    if not self.slug:
+      self.slug = slugify(self.name)
+    return super(Product,self).save(*args, **kwargs)
+  
+  
+
 class Store(models.Model):
-  product = models.OneToOneField(Product,on_delete=models.CASCADE)
+  product = models.ManyToManyField(Product,blank=True)
   name=models.CharField(max_length=50,blank=True)
+  slug = models.SlugField(unique=True, blank=True, null=True)
   details = models.TextField(blank=True)
   cover = models.ImageField(upload_to="shop/cov")
 
+  class Meta:
+      ordering = ['-name']
 
   
   def __str__(self) :
     return self.name
+  
+  def save(self,*args, **kwargs):
+    if not self.slug:
+      self.slug = slugify(self.name)
+    return super(Store,self).save(*args, **kwargs)
