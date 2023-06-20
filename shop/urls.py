c1cb8dd1da704a5709e143460096d48e0387ec0c
Talha Khan <123312136+talhakhan54@users.noreply.github.com>
2023-06-20 12:23:41 +0600

    __

--- shop/urls.py
+++ shop/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from shop.views import list_product_view
+from shop.views import list_product_view,product_details_view,create_product_view,update_product_view,seller
 urlpatterns = [
     path('',list_product_view,name="pro_list"),
+    path('pro/sell/',seller,name="pro_sell"),
+    path('pro/add/',create_product_view,name="pro_add"),
+    path('pro/<str:slug>/',product_details_view,name="pro_det"),
+    path('pro/upd/<str:slug>/',update_product_view,name="pro_upd"),
+    path('pro/dlt/<str:slug>/',update_product_view,name="pro_upd"),
 ]
 
 app_name = "shop"
--- shop/views.py
+++ shop/views.py
@@ -16,10 +16,10 @@ def list_product_view(request):
 
 
 
-def post_details(request,slug):
-  post=Product.objects.get(slug=slug)
+def product_details_view(request,slug):
+  product=Product.objects.get(slug=slug)
   context={
-    "post":post
+    "pro":product
   }
   return render(request,'shop/prodet.html',context)
 
@@ -27,7 +27,7 @@ def post_details(request,slug):
 
 
 @login_required(login_url='login')
-def create_post(request):
+def create_product_view(request):
   form=ProductCreateForm()
   context={
     "form":form
@@ -35,11 +35,11 @@ def create_post(request):
   if request.method =="POST":
     form=ProductCreateForm(request.POST, request.FILES)
     if form.is_valid():
-      post =form.save(commit=False)
-      post.author = request.user
-      post.save()
-      return redirect("/blog")
-  return render(request, 'blog/addpost.html',context)
+      pro =form.save(commit=False)
+      pro.author = request.user
+      pro.save()
+      return redirect("/shop")
+  return render(request, 'shop/addprod.html',context)
 
 
 
@@ -48,32 +48,34 @@ def create_post(request):
 
 
 @login_required(login_url='login')
-def update_post(request,slug):
-  post=Product.objects.get(slug=slug)
-  form=ProductCreateForm(instance=post)
+def update_product_view(request,slug):
+  product=Product.objects.get(slug=slug)
+  form=ProductCreateForm(instance=product)
   context={
     "form":form,
   }
-  if not post.can_edit(request.user):
-        messages.error(request,"You don't have permission to update others post")
-        return redirect('/blog')
+  if not product.can_edit(request.user):
+        messages.error(request,"You don't have permission to update Product.")
+        return redirect('/shop')
   if request.method == "POST" :
-    form=ProductCreateForm(request.POST, instance=post, files=request.FILES)
+    form=ProductCreateForm(request.POST, instance=product, files=request.FILES)
     if form.is_valid():
-      post =form.save(commit=False)
-      post.author = request.user
-      post.save()
-      return redirect("/blog")
+      product =form.save(commit=False)
+      product.author = request.user
+      product.save()
+      return redirect("/shop")
 
-  return render(request, "blog/updatepost.html",context)
+  return render(request, "shop/updpro.html",context)
 
 @login_required(login_url='login')
-def delt_post(request,slug):
-  post=Product.objects.get(slug=slug)
-  if not post.can_edit(request.user):
+def delt_product(request,slug):
+  product=Product.objects.get(slug=slug)
+  if not product.can_edit(request.user):
         messages.error(request,"You don't have permission to delete others post")
-        return redirect('/blog')
+        return redirect('/shop')
   else:
-    post.delete()
-  return render(request, 'blog/dltpost.html')
+    product.delete()
+  return render(request, 'shop/dltpro.html')
 
+def seller(request):
+   return render(request, "shop/seller.html")
